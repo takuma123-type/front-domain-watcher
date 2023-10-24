77cb248814b52c81ac3caa088503a60aa841fed4
Takuma <>
2023-10-25 01:23:03 +0900

    userの表示確認

--- admin_front/src/components/molecules/users/TableRow.tsx
+++ admin_front/src/components/molecules/users/TableRow.tsx
@@ -6,7 +6,7 @@ export interface User {
   lastName: string;
   attribute: string;
   email: string;
-  status: string;
+  status?: string;
   verified?: boolean;
 }
 
--- admin_front/src/components/pages/Index.tsx
+++ admin_front/src/components/pages/Index.tsx
@@ -5,14 +5,14 @@ import Header from "../molecules/shared/Header";
 import Pagination from "../organisms/shared/Pagination";
 import TableHeader from "../molecules/users/TableHeader";
 import TableRow from "../molecules/users/TableRow";
-// import useUserRepository from "../../infrastructure/repositories/UsersRepository";
 import { Storage } from "../../infrastructure/Storage";
+import axios from "axios";
 
 interface User {
-  attribute: string;
   id: number;
   firstName: string;
   lastName: string;
+  attribute: string;
   email: string;
 }
 
@@ -27,8 +27,6 @@ const meta = {
 export default function Users() {
   const navigate = useNavigate();
   const sessionToken = Storage.restoreSessionToken();
-  // const userRepository = useUserRepository(sessionToken);
-
   const handleCellClick = (userId: number) => {
     navigate(`/users/details/${userId}`);
   };
@@ -56,25 +54,27 @@ export default function Users() {
   const [currentJobSeekerUsers, setCurrentJobSeekerUsers] = useState<User[]>(
     []
   );
-  const [users, setUsers] = useState<any[]>([]); // ユーザーデータの状態を管理
-  const [loading, setLoading] = useState<boolean>(true); // データロード中のフラグ
+  const [users, setUsers] = useState<any[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
-
-  // useEffect(() => {
-  //   userRepository.fetchUsers();
-  // }, []);
-
   useEffect(() => {
-    // データ取得関数
     const fetchData = async () => {
       try {
-        const response = await fetch("http://localhost:3000/admin/users");
-        if (!response.ok) {
-          const errorData = await response.json(); // Try to parse error response
-          throw new Error(errorData.message || "Failed to fetch data.");
-        }
-        const data = await response.json();
-        setUsers(data);
+        const response = await axios.get("http://localhost:3000/admin/users", {
+          headers: {
+            Accept: "application/json",
+          },
+        });
+        setUsers(
+          response.data.results.map((user: any) => ({
+            id: user.id,
+            firstName: user.name.split(" ")[0],
+            lastName: user.name.split(" ")[1] || "",
+            attribute: user.status,
+            email: user.email,
+          }))
+        );
+        console.log(response.data.results);
       } catch (err) {
         console.error(err);
       } finally {
@@ -86,10 +86,10 @@ export default function Users() {
   }, []);
 
   useEffect(() => {
-    const startIndex = (currentPage - 1) * itemsPerPage;
-    const endIndex = startIndex + itemsPerPage;
-    setCurrentUsers(filteredUsers.slice(startIndex, endIndex));
-  }, [currentPage, itemsPerPage, filteredUsers]);
+    const offset = (currentPage - 1) * itemsPerPage;
+    const currentData = filteredUsers.slice(offset, offset + itemsPerPage);
+    setCurrentUsers(currentData);
+  }, [filteredUsers, currentPage, itemsPerPage]);
 
   return (
     <React.Fragment>
@@ -139,108 +139,6 @@ export default function Users() {
                 <Pagination
                   currentPage={currentPage}
                   totalPages={totalPages}
-                  onPageChange={handlePageChange}
-                />
-              </div>
-            </div>
-          </div>
-        </section>
-        <section className="py-4 overflow-hidden">
-          <div className="container px-4 mx-auto">
-            <div className="py-5 bg-neutral-50 border border-neutral-100 rounded-xl">
-              <div className="px-6">
-                <h3 className="font-heading pb-8 text-lg text-neutral-600 font-semibold">
-                  求職者一覧
-                  <input
-                    className="pl-2 py-3 text-sm text-gray-200 border ml-2.5"
-                    type="text"
-                    placeholder="ユーザ検索"
-                    value={searchQuery}
-                    onChange={(e) => setSearchQuery(e.target.value)}
-                  />
-                </h3>
-                <div className="mb-5 w-full overflow-x-auto">
-                  <table className="w-full min-w-max table-auto">
-                    <thead>
-                      <tr className="text-left">
-                        <TableHeader label="Id" />
-                        <TableHeader label="性" />
-                        <TableHeader label="名" />
-                        <TableHeader label="属性" />
-                        <TableHeader label="メールアドレス" />
-                        <th className="pb-3.5 border-b border-neutral-100" />
-                      </tr>
-                    </thead>
-                    <tbody>
-                      {currentJobSeekerUsers.map((user) => (
-                        <TableRow
-                          key={user.id}
-                          id={user.id}
-                          firstName={user.firstName}
-                          lastName={user.lastName}
-                          attribute={user.attribute}
-                          email={user.email}
-                          onCellClick={() => handleCellClick(user.id)}
-                          status={""}
-                        />
-                      ))}
-                    </tbody>
-                  </table>
-                </div>
-                <Pagination
-                  currentPage={currentPage}
-                  totalPages={totalJobSeekerPages}
-                  onPageChange={handlePageChange}
-                />
-              </div>
-            </div>
-          </div>
-        </section>
-        <section className="py-4 overflow-hidden">
-          <div className="container px-4 mx-auto">
-            <div className="py-5 bg-neutral-50 border border-neutral-100 rounded-xl">
-              <div className="px-6">
-                <h3 className="font-heading pb-8 text-lg text-neutral-600 font-semibold">
-                  OG一覧
-                  <input
-                    className="pl-2 py-3 text-sm text-gray-200 border ml-2.5"
-                    type="text"
-                    placeholder="ユーザ検索"
-                    value={searchQuery}
-                    onChange={(e) => setSearchQuery(e.target.value)}
-                  />
-                </h3>
-                <div className="mb-5 w-full overflow-x-auto">
-                  <table className="w-full min-w-max table-auto">
-                    <thead>
-                      <tr className="text-left">
-                        <TableHeader label="Id" />
-                        <TableHeader label="性" />
-                        <TableHeader label="名" />
-                        <TableHeader label="属性" />
-                        <TableHeader label="メールアドレス" />
-                        <th className="pb-3.5 border-b border-neutral-100" />
-                      </tr>
-                    </thead>
-                    <tbody>
-                      {currentOGUsers.map((user) => (
-                        <TableRow
-                          key={user.id}
-                          id={user.id}
-                          firstName={user.firstName}
-                          lastName={user.lastName}
-                          attribute={user.attribute}
-                          email={user.email}
-                          onCellClick={() => handleCellClick(user.id)}
-                          status={""}
-                        />
-                      ))}
-                    </tbody>
-                  </table>
-                </div>
-                <Pagination
-                  currentPage={currentPage}
-                  totalPages={totalOGPages}
                   onPageChange={handlePageChange}
                 />
               </div>
